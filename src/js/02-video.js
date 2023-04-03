1bfc18fb9d924a6e4cde6489d79b61efadfec236
Adrian Miarczyński <adrian331144@gmail.com>
2023-04-03 15:34:24 +0200

    finish second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+const timeUpdate = throttle(
+  time => localStorage.setItem('videoplayer-current-time', time.seconds),
+  1000
+);
+// player.on('play', function() {
+//   throttle(
+//     time => localStorage.setItem('videoplayer-current-time', time.seconds),
+//     1000
+//   );
+//   // localStorage.setItem('videoplayer-current-time', ev.seconds);
+// });
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.on('timeupdate', timeUpdate);
